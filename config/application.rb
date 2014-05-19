474a4f0b6874e1817336522d9e130adcd52c5a16
walkeriniraq <nathan@rylath.net>
2014-05-18 19:10:34 -0500

    Removed old config info

--- config/application.rb
+++ config/application.rb
@@ -26,10 +26,5 @@ module JpgRanger
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.db = {
-      host: APP_CONFIG['DATABASE'][Rails.env]['HOST'],
-      db: APP_CONFIG['DATABASE'][Rails.env]['DB']
-    }
-
   end
 end
