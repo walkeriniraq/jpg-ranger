a6c2c0c91e6e5eb4c975dc878046d9a8e23d7275
walkeriniraq <nathan@rylath.net>
2014-05-25 13:23:07 -0500

    Tag page displays

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,13 +2,16 @@ class PhotosController < ApplicationController
 
   respond_to :json
 
-  def index
-    # find all
-    respond_with Photo.all
+  def index # find all
+    query = Photo.order_by(:upload_time.desc).limit(20)
+    tag = params[:tag].andand.downcase.andand.strip
+    unless tag.blank?
+      query = query.where(:tags.in => [tag])
+    end
+    respond_with query
   end
 
-  def show
-    # find
+  def show # find
     respond_with Photo.find(params[:id])
   end
 
