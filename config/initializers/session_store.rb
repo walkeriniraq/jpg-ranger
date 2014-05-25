1a745949aecadceee4a8c1ffdbce6d4de4b5757d
walkeriniraq <nathan@rylath.net>
2014-05-25 13:18:21 -0500

    Moved to a more descriptive initializer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,5 +2,3 @@
 
 JpgRanger::Application.config.session_store :cookie_store, key: '_jpg-ranger_session'
 
-Mongoid::Document.send(:include, ActiveModel::SerializerSupport)
-Mongoid::Criteria.delegate(:active_model_serializer, to: :to_a)
